6766082c1382c161df63ec603f468289d3f8783e
mido <onex911@hotmail.com>
2021-12-15 20:53:17 +0200

    keyboard press

--- src/KeyPad.js
+++ src/KeyPad.js
@@ -1,5 +1,6 @@
 
 
+import { click } from "@testing-library/user-event/dist/click";
 import { useEffect } from "react";
 import clap from "./wav/clap.wav";
 import eleckick from "./wav/elec-kick.wav";
@@ -13,12 +14,19 @@ import tom from "./wav/tom.wav";
 function KeyPad (){
 
     function playTonehandle(event){
+        console.log(event.target);
         document.getElementById(event.target.innerHTML[0]).play();
     }
   
    useEffect(()=>{
     document.addEventListener('keydown',(e)=>{
-        e.key==="q"?console.log('the right key'):console.log('nope')
+        const keysArr =["q","w","e","a","s","d","z","x","c"];
+        keysArr.forEach((val)=>{
+            if(e.key.toLowerCase()===val){
+                document.getElementById(val).click();
+
+            }
+        })
     });
    },[])
     
@@ -28,31 +36,31 @@ function KeyPad (){
             
             
             
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>Q
+            <button id="q" className="drum-pad" onClick={(e)=>playTonehandle(e)}>Q
             <audio id="Q" className="clip" src={clap}/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>W
+            <button id="w" className="drum-pad" onClick={(e)=>playTonehandle(e)}>W
             <audio id="W" src={eleckick} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>E
+            <button id="e" className="drum-pad" onClick={(e)=>playTonehandle(e)}>E
             <audio id="E" src={hat} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>A
+            <button id="a" className="drum-pad" onClick={(e)=>playTonehandle(e)}>A
             <audio id="A" src={kick} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>S
+            <button id="s" className="drum-pad" onClick={(e)=>playTonehandle(e)}>S
             <audio id="S" src={ride} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>D
+            <button id="d" className="drum-pad" onClick={(e)=>playTonehandle(e)}>D
             <audio id="D" src={snare} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>Z
+            <button id="z" className="drum-pad" onClick={(e)=>playTonehandle(e)}>Z
             <audio id="Z" src={splash} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>X
+            <button id="x" className="drum-pad" onClick={(e)=>playTonehandle(e)}>X
             <audio id="X" src={tabla} className="clip"/>
             </button>
-            <button className="drum-pad" onClick={(e)=>playTonehandle(e)}>C
+            <button id="c" className="drum-pad" onClick={(e)=>playTonehandle(e)}>C
             <audio id="C" src={tom} className="clip"/>
             </button>
         </div>
